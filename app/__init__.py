89ee5c9ee990112c5f9bbe12cd70e49bb2579e39
roaming97 <roaming98a@gmail.com>
2021-09-22 18:03:54 -0500

    Template reinforcement and static file delivery

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__, template_folder='html')
+app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///almond.db'
 db = SQLAlchemy(app)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,22 @@
 from flask import render_template
+from datetime import datetime
 from App import app
+
+dummy = [
+    {
+        'title': 'sample video 1',
+        'author': 'myself',
+        'description': 'my video',
+        'date': datetime.strptime("01/01/2009","%m/%d/%Y").strftime('%m/%d/%Y')
+    },
+    {
+        'title': 'sample video 2',
+        'author': 'myself again',
+        'description': 'my second video',
+        'date': datetime.strptime("11/04/2012","%m/%d/%Y").strftime('%m/%d/%Y')
+    },
+]
+
 
 # Routes
 
