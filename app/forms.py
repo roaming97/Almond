870b101c07a646530fc292cb4cb53dbb64eb7cf9
roaming97 <roaming98a@gmail.com>
2021-09-24 21:07:56 -0500

    Add 'URL' validator to url in quick add form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, URL
 
 
 class QuickAddForm(FlaskForm):
-    url = StringField('YouTube URL', validators=[DataRequired(), Length(min=28, max=43)])
+    url = StringField('YouTube URL', validators=[DataRequired(), Length(min=28, max=43), URL()])
     submit = SubmitField('Add Video')
