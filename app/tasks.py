06095bbaa1f95b8f25755e71851f79a3be7370a4
MetadataVinter <MetadataVinter@users.noreply.github.com>
2021-10-02 20:55:05 -0300

    2

--- app/tasks.py
+++ app/tasks.py
@@ -1,4 +1,5 @@
 import re
+import httpx
 import urllib.request as r
 from base64 import b64encode
 from os import getenv, remove
@@ -50,6 +51,7 @@ def register_data(**kwargs):
 def save_blobs(with_video=True, **kwargs):
     blobs = []
     thumb_path = f'{kwargs["vid_id"]}.{kwargs["thumb_ext"]}'
+    pfp_path = f'{kwargs["vid_id"]}.{kwargs["pfp_ext"]}'
     v_title = str(kwargs["vid_title"]) \
         .replace('"', "'") \
         .replace(':', ' -') \
@@ -58,15 +60,20 @@ def save_blobs(with_video=True, **kwargs):
     video_path = f'{v_title}-{kwargs["vid_id"]}.{kwargs["vid_ext"]}'
 
     thumb_file = r.urlretrieve(kwargs['thumb_url'], thumb_path)
+    pfp_file = r.urlretrieve(kwargs['pfp_url'], pfp_path)
     with open(thumb_file[0], 'rb') as t:
         blobs.append(t.read())
     if with_video:
         with open(video_path, 'rb') as v:
             blobs.append(v.read())
+    with open(pfp_file[0], 'rb') as p:
+        blobs.append(p.read())
     if isfile(thumb_path):
         remove(thumb_path)
     if isfile(video_path):
         remove(video_path)
+    if isfile(pfp_path):
+        remove(pfp_path)
     return blobs
 
 
@@ -94,9 +101,23 @@ def quick_add(url: str, with_blobs=True):
         date = info.get('upload_date', None)
         likes = info.get('like_count', 'N/A')
         dislikes = info.get('dislike_count', 'N/A')
-        subscribers = info.get('subscribers', 'N/A')  # Unknown location for subscribers data
         thumbnail_url = str(info.get('thumbnails', None)[0]['url']).split("?")[0]
-        profile_picture = info.get('profile_picture', None)  # Insert profile picture scrap here
+
+        raw_html = httpx.get(author_url)
+        pfp_regex = r'((avatar":{"thumbnails":\[{"url":")(https(.+?))(s48))'
+        subscribers_regex = r'("},"subscriberCountText":{"accessibility":{"accessibilityData":{"label":"(.+?)"}},(.+?)"},"t)'
+        subscribers_regex_2 = r'}}},"trackingParams":"(.+?)(="}},"subscriberCountText":{"accessibility":{"accessibilityData":{"label":"(.+?)"}},(.+?)"})'
+        profile_picture = "N/A"
+        subscribers = "N/A"
+
+        for profile_picture_search in re.finditer(pfp_regex, str(raw_html.text)):
+            profile_picture = profile_picture_search.group(3) + "s1000-c-k-c0x00ffffff-no-rj"
+
+        for subscribers_count_search in re.finditer(subscribers_regex, str(raw_html.text)):
+            subscribers = subscribers_count_search.group(2)
+            
+        for subscribers_count_search in re.finditer(subscribers_regex_2, str(raw_html.text)):
+            subscribers = subscribers_count_search.group(3)
 
         if with_blobs:
             blobs = save_blobs(
@@ -104,10 +125,13 @@ def quick_add(url: str, with_blobs=True):
                 thumb_ext=(str(thumbnail_url).split(".")[-1]).split('?')[0],
                 vid_id=video_id,
                 vid_title=title,
-                vid_ext=info["ext"]
+                vid_ext=info["ext"],
+                pfp_url=profile_picture,
+                pfp_ext=".png"
             )
             thumbnail = b64encode(blobs[0])
             stream = b64encode(blobs[1])
+            profile_picture = b64encode(blobs[2])
         else:
             thumbnail = b''
             stream = b''
